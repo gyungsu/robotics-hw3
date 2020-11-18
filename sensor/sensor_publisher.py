2882f237dfa9cfa2bd1f960656bca459c4f0ecd9
gyungsu <kew27@naver.com>
2020-11-18 22:59:48 +0900

    write  message file

--- sensor/sensor_publisher.py
+++ sensor/sensor_publisher.py
@@ -5,8 +5,8 @@ from geometry_msgs.msg import Vector3
 from common_msgs.msg import Random
 
 
-rospy.init_node('custom_publisher')
-pub = rospy.Publisher('custom_msg', Random, queue_size=1)
+rospy.init_node('sensor_publisher')
+pub = rospy.Publisher('sensor_msg', Random, queue_size=1)
 msg = Random()
 rate = rospy.Rate(1)
 while not rospy.is_shutdown():   
@@ -14,6 +14,7 @@ while not rospy.is_shutdown():
     second = msg.timestamp.secs
     msg.pose = Vector3(x = random.randint(1,100), y = random.randint(1,100), z = 0)
     msg.pose.z = msg.pose.x + msg.pose.y
+    print("Time : ", msg.timestamp.secs%100)
     print("x + y = z")
     print(msg.pose.x, end = "")
     print("+", msg.pose.y, end = "")
