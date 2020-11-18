1b1f93f16e540cfb159a69ded6f35722d1024805
gyungsu <kew27@naver.com>
2020-11-18 23:38:15 +0900

    write  message file

--- algorithm/print_subscriber.py
+++ algorithm/print_subscriber.py
@@ -6,8 +6,8 @@ def callback(msg):
     print("Time : ", msg.timestamp.secs%100)
     print("x + y = z")
     print(msg.pose.x, end = "")
-    print("+", msg.pose.y, end = "")
-    print("=", msg.pose.z)
+    print(" +", msg.pose.y, end = "")
+    print(" =", msg.pose.z)
 
 rospy.init_node('algorithm_subscriber')
 sub = rospy.Subscriber('sensor_msg', Random, callback)
