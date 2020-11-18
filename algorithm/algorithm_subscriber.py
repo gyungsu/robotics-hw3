f67bf71769ff53e602ce364e360eb30cd8963b0b
gyungsu <kew27@naver.com>
2020-11-18 23:00:50 +0900

    write  message file

--- algorithm/algorithm_subscriber.py
+++ algorithm/algorithm_subscriber.py
@@ -5,6 +5,6 @@ from common_msgs.msg import Random
 def callback(msg):
     print ("subscribe:", msg.pose.x, msg.pose.y, msg.pose.z)
 
-rospy.init_node('custom_subscriber')
-sub = rospy.Subscriber('custom_msg', Random, callback)
+rospy.init_node('algorithm_subscriber')
+sub = rospy.Subscriber('algorithm_msg', Random, callback)
 rospy.spin()
