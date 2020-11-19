362bb271c12a4ef1bcd2050cc825d6242114efcd
gyungsu <kew27@naver.com>
2020-11-19 23:26:48 +0900

    write service

--- sensor/BMI_client.py
+++ sensor/BMI_client.py
@@ -6,7 +6,7 @@ from common_msgs.srv import BMI, BMIRequest
 rospy.init_node('BMI_client')
 rospy.wait_for_service('bmi')
 requester = rospy.ServiceProxy('bmi', BMI)
-print ("requester type:", type(requester), ", callable?", callable(requester))
+
 rate = rospy.Rate(10)
 count = 0
 while count < 100:
